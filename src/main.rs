c002b38ef184319e06360432c990946389b6abe9
tczyz <tomczyz97@gmail.com>
2025-09-12 23:48:01 +0200

    Integrate `env_logger` for improved logging and track application startup time in `main.rs`.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use crate::tui::App;
 use clap::Parser;
 use color_eyre::{Report, Result};
 use directories::ProjectDirs;
+use log::info;
 use once_cell::sync::Lazy;
 use std::borrow::Cow;
+use tokio::time::Instant;
 
 static PROJECT_DIRS: Lazy<ProjectDirs> = Lazy::new(|| {
     ProjectDirs::from("com", "example", "tag-tool-cli")
@@ -18,13 +20,20 @@ static PROJECT_DIRS: Lazy<ProjectDirs> = Lazy::new(|| {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let startup_instant = Instant::now();
     // force early init of the project dirs to handle panic
     let _ = PROJECT_DIRS.data_dir();
+
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .format_timestamp_millis()
+        .init();
 
     let cli = Cli::parse();
 
     let storage: &'static Storage = Box::leak(Box::new(Storage::initialize().await));
     let app = App::with_storage(&storage);
+
+    info!("startup time: {:?}", startup_instant.elapsed());
 
     match &cli.command {
         Commands::Tag { path, tags } => {
