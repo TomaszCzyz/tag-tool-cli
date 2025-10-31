647e818a60d9642ea8499df1c047045d738c4fd6
tczyz <tomczyz97@gmail.com>
2025-10-31 18:30:31 +0100

    Integrate `tui-input` for input handling, add fuzzy tag matching with SkimMatcherV2, and update UI layout with input box, messages, and tag suggestions.

--- src/tui.rs
+++ src/tui.rs
@@ -1,69 +1,135 @@
-use crossterm::event;
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
-use ratatui::layout::{Constraint, Direction, Layout};
-use ratatui::prelude::{Line, Stylize};
-use ratatui::widgets::{Block, Borders, List, ListItem, Paragraph};
-use ratatui::{DefaultTerminal, Frame};
+use color_eyre::owo_colors::OwoColorize;
+use fuzzy_matcher::FuzzyMatcher;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use ratatui::DefaultTerminal;
+use ratatui::crossterm::event;
+use ratatui::crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::prelude::*;
+use ratatui::widgets::{Block, List, Paragraph};
+use std::iter;
+use tui_input::Input;
+use tui_input::backend::crossterm::EventHandler;
 
 /// The main application which holds the state and logic of the application.
-#[derive(Debug)]
 pub struct App {
     /// Is the application running?
     running: bool,
+    /// Current value of the input box
+    input: Input,
+    /// History of recorded messages
+    messages: Vec<String>,
+
+    tags: Vec<String>,
+
+    matcher: Box<dyn FuzzyMatcher>,
 }
 
 impl App {
     /// Construct a new instance of [`App`].
     pub fn new() -> Self {
-        Self { running: false }
+        let tags = std::fs::read_to_string("test-tags.txt")
+            .map(|content| {
+                content
+                    .replace('\n', "")
+                    .split(',')
+                    .map(|s| s.trim().to_string())
+                    .filter(|s| !s.is_empty())
+                    .collect()
+            })
+            .unwrap_or_default();
+
+        Self {
+            running: false,
+            matcher: Box::new(SkimMatcherV2::default()),
+            tags,
+            input: Input::default(),
+            messages: vec![],
+        }
     }
 
     /// Run the application's main loop.
     pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
-        self.running = true;
-        while self.running {
+        loop {
             terminal.draw(|frame| self.render(frame))?;
-            self.handle_crossterm_events()?;
+
+            let event = event::read()?;
+            if let Event::Key(key) = event {
+                match key.code {
+                    KeyCode::Enter => (),
+                    KeyCode::Esc => return Ok(()),
+                    _ => {
+                        self.input.handle_event(&event);
+                    }
+                }
+            }
         }
-        Ok(())
     }
 
-    /// Renders the user interface.
-    ///
-    /// This is where you add new widgets. See the following resources for more information:
-    ///
-    /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
-    /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
-    fn render(&mut self, frame: &mut Frame) {
-        let size = frame.area();
+    fn push_message(&mut self) {
+        self.messages.push(self.input.value_and_reset());
+    }
 
-        // Create a small region near the top (like fzf)
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(3),  // Input line
-                Constraint::Length(10), // Suggestion list
-                Constraint::Min(0),     // Remaining area (empty)
-            ])
-            .split(size);
+    fn render(&self, frame: &mut Frame) {
+        let [header_area, input_area, messages_area] =
+            Layout::vertical([Constraint::Length(1), Constraint::Length(3), Constraint::Min(1)]).areas(frame.area());
 
-        let items = vec![
-            "src",
-            "src/migrations",
-            "src/event_sourcing",
-            "src/event_sourcing/sqlite_store/sql/statements",
-            "target/debug/incremental/tagtool",
-        ];
+        self.render_tags_suggestions(frame, header_area);
+        self.render_input(frame, input_area);
+        self.render_messages(frame, messages_area);
+    }
 
-        // Draw input field
-        let input = Paragraph::new("query.as_ref()").block(Block::default().borders(Borders::ALL).title("Search"));
-        frame.render_widget(input, chunks[0]);
+    fn render_tags_suggestions(&self, frame: &mut Frame, area: Rect) {
+        let mut score_tags = self
+            .tags
+            .iter()
+            .filter_map(|tag| match self.matcher.fuzzy_indices(tag, self.input.value()) {
+                Some(result) => Some((result, tag)),
+                None => None,
+            })
+            .collect::<Vec<_>>();
 
-        // Draw fuzzy results (here: static)
-        let list_items: Vec<ListItem> = items.iter().map(|i| ListItem::new(i.to_string())).collect();
-        let list = List::new(list_items).block(Block::default().borders(Borders::ALL).title("Results"));
+        score_tags.sort_unstable_by_key(|((s, _), _)| 100 - *s);
 
-        frame.render_widget(list, chunks[1]);
+        // TODO: calculate number based on number of chars and frame width
+        let count = 20;
+
+        let help_message = Line::from_iter(score_tags.into_iter().take(count).flat_map(|((_, indices), tag)| {
+            let map = tag.chars().enumerate().map(move |(idx, ch)| {
+                let s = ch.to_string();
+                if indices.contains(&idx) {
+                    Span::styled(s, Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD))
+                } else {
+                    Span::raw(s)
+                }
+            });
+
+            Line::from_iter(map.chain(iter::once(Span::raw(" "))))
+        }));
+
+        frame.render_widget(help_message, area);
+    }
+
+    fn render_input(&self, frame: &mut Frame, area: Rect) {
+        // keep 2 for borders and 1 for cursor
+        let width = area.width.max(3) - 3;
+        let scroll = self.input.visual_scroll(width as usize);
+        let style: Style = Color::Yellow.into();
+        let input = Paragraph::new(self.input.value())
+            .style(style)
+            .scroll((0, scroll as u16))
+            .block(Block::bordered().title("Input"));
+        frame.render_widget(input, area);
+
+        // Ratatui hides the cursor unless it's explicitly set. Position the cursor past the
+        // end of the input text and one line down from the border to the input line
+        let x = self.input.visual_cursor().max(scroll) - scroll + 1;
+        frame.set_cursor_position((area.x + x as u16, area.y + 1))
+    }
+
+    fn render_messages(&self, frame: &mut Frame, area: Rect) {
+        let messages = self.messages.iter().enumerate().map(|(i, message)| format!("{}: {}", i, message));
+        let messages = List::new(messages).block(Block::bordered().title("Messages"));
+        frame.render_widget(messages, area);
     }
 
     /// Reads the crossterm events and updates the state of [`App`].
